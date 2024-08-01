edd2997084845df45e7f1589c8aba10980c91bba
Vinícius Batista <Viniciusdias294@gmail.com>
2024-08-01 17:30:15 -0300

    Atualização, listar por email em ordem alfabética

--- consulta.php
+++ consulta.php
@@ -23,4 +23,17 @@
     } else {
         echo "Nenhum registro encontrado!";
     }
+    echo"<BR>Lista ordenada por email<br>";
+    $sql = "SELECT id, nome, senha, email FROM usuario order by email";
+    $resultado = mysqli_query($conexao, $sql);
+    echo "<br>";
+    if (mysqli_num_rows($resultado) > 0){
+        while ($registro = mysqli_fetch_assoc($resultado)){
+            echo "id: " . $registro["id"]. " nome: " . $registro["nome"].
+            " senha: ". $registro["senha"]. "email:". $registro["email"]. "<br>";
+        }
+    } else {
+        echo "Nenhum registro encontrado!";
+    }
     ?>
+    
