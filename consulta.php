bbc9bb3fd37862cb0ba6b1aba8eab9a4e3c2d294
Vinícius Batista <Viniciusdias294@gmail.com>
2024-08-01 17:26:07 -0300

    Parte 2

--- consulta.php
+++ consulta.php
@@ -11,4 +11,16 @@
     } else {
         echo "Nenhum registro encontrado!";
     }
+    echo"<BR>Nomes que começam com 'c'<br>";
+    $sql = "SELECT id, nome, senha, email FROM usuario where nome like 'c%'";
+    $resultado = mysqli_query($conexao, $sql);
+    echo "<br>";
+    if (mysqli_num_rows($resultado) > 0){
+        while ($registro = mysqli_fetch_assoc($resultado)){
+            echo "id: " . $registro["id"]. " nome: " . $registro["nome"].
+            " senha: ". $registro["senha"]. "email:". $registro["email"]. "<br>";
+        }
+    } else {
+        echo "Nenhum registro encontrado!";
+    }
     ?>
